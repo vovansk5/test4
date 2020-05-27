c24c139a2c2f6522c1cfffc816ca9c84a4d82b99
vovansk5 <vovansk5@gmail.com>
2020-05-27 21:59:33 +0700

    1

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,11 @@
     <br>
     {{HT}}
     <br>
-    <button @click="loadHT">Загрузим хэштеги</button>
+    <button @click="loadHT">Загрузим loadHT</button>
+    <br>
+    <button @click="MyFetch">Загрузим MyFetch</button>
+    <br>
+    <button @click="MyXHR">Загрузим MyXHR</button>
 
   </div>
 </template>
@@ -24,19 +28,38 @@ export default {
       HT:[],
       url: {
         HT:'https://dka-develop.ru/api?type=hashtag',
-        Citys:'https://dka-develop.ru/api?type=city'
+        Citys:'https://dka-develop.ru/api?type=city',
+        DronRef:'https://api.dron.digital/apee/referrals-f88a20cc-757e-11ea-8c9a-00155d020612',
+        DronCitys:'http://cms.rc.dron.digital/api/v1/cities?country_id=1',
+        BB:'https://bookingboard.ru/plan/?order_id=6D037E09-C536-483A-9979-A0145410F688&city_id=1'
       }
     }
   },
   methods: {
       loadHT() {
         this.i++;
-        console.log(this.url.HT);
-        axios.get(this.url.HT).then( (response)=> {
+        axios.get(this.url.DronRef).then( (response)=> {
           this.HT=response.data;
         });
         this.i++;
+      },
+      myPost() {
+        {
+          axios.post(this.url.BB, {
+              "action": "books_get",
+              "params": {
+                "tableName": "CorpRD",
+                "searchText": "ива"
+              },
+              "token": "7E7E8B19-9CD6-4DB4-B510-0DFE48DB327B",
+              "version": 1
+          }).then ((response)=> {
+            this.HT=response.data;
+          })
+}
       }
+
+
   }
 }
 </script>
